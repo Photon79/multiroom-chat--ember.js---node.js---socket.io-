eb6629aa56b4d7fe416fead33021217a4dca9f0c
Mezentsev Sergey <photon79@gmail.com>
2013-10-29 21:15:06 +0400

    Remove common/db.js from git

--- common/db.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(mongoose) {
-	mongoose.connect('mongodb://localhost:27017/chat');
-
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error: '));
-	db.once('open', function() {
-		console.log('Successful connect to db');
-	});
-};
