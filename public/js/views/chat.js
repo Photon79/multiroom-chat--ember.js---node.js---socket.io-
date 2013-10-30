100ca48fe02a0b6c344a5af27f141789a6afb124
Mezentsev Sergey <photon79@gmail.com>
2013-10-30 18:46:11 +0400

    Start create chat room component

--- public/js/views/chat.js
+++ public/js/views/chat.js
@@ -1,11 +1,14 @@
 Chat.CreateRoomFormComponent = Em.Component.extend({
 	actions: {
 		submit: function() {
-			$('#createRoom .close').click();	
+			$('#createRoom .close').click();
 			this.sendAction('submit', {
 				title: this.get('title'),
 				description: this.get('description')
 			});
 		}
 	}
+});
+Chat.RoomWindow = Em.Component.extend({
+	tagName: 'div'
 });
